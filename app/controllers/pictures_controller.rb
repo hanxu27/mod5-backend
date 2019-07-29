e97777152178a9326fe9fbbe848c1f15416fda8e
Han Xu <hanxu27@gmail.com>
2019-07-29 14:06:37 -0400

    flickr pictures v0

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,21 +12,23 @@ class PicturesController < ApplicationController
   # end
 
   def flickr_pictures
-    sort = '&sort=relevance'
+    sort = '&sort=interestingness-asc'
     url_flickr = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json'
     api_key = "&api_key=#{ENV['FLICKR_API_KEY']}"
-    extras = '&extras=url_h,url_l'
+    extras = '&extras=url_h,url_l,url_b,url_c,url_z,url_m'
     tags = "&tags=#{params[:tag]}"
     per_page = '&per_page=15'
     page = "&page=#{params[:page] || 1}"
-    pictures = JSON.parse(open(url_flickr + api_key + extras + tags + per_page + page).read.split('(')[1].split(')')[0])['photos']['photo']
+    p(url_flickr + api_key + extras + tags + per_page + page + sort)
+    pictures = JSON.parse(open(url_flickr + api_key + extras + tags + per_page + page + sort).read[14..-2])['photos']['photo']
     display_pictures = pictures.map do |p|
       picture = {}
       picture[:id] = p['id']
       picture[:caption] = p['title']
-      picture[:src] = p['url_h'] || p['url_l']
-      picture[:width] = (p['width_h'] || p['width_l']).to_i
-      picture[:height] = (p['height_h'] || p['height_l']).to_i
+      picture[:src] = p['url_h'] || p['url_l'] || p['width_b'] || p['url_c'] || p['url_z'] || p['url_m']
+      picture[:width] = (p['width_h'] || p['width_l'] || p['width_b'] || p['width_c'] || p['width_z'] || p['width_m']).to_f
+      picture[:height] = (p['height_h'] || p['height_l'] || p['width_b'] || p['height_c'] || p['height_z'] || p['height_m']).to_f
+      # byebug
       picture
     end
     render json: display_pictures, status: :ok
