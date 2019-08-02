bcc2a0c91dee150632b6958d5141f82cab2d7d94
Han Xu <hanxu27@gmail.com>
2019-08-02 10:06:27 -0400

    updated cors origin https://park-browser-frontend.herokuapp.com/ into array

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://park-browser-frontend.herokuapp.com/'
+    origins ['https://park-browser-frontend.herokuapp.com/']
 
     resource '*',
       headers: :any,
