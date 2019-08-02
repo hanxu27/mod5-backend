ada5e99f729afd00bc409521c1556642481d18c2
Han Xu <hanxu27@gmail.com>
2019-08-01 21:33:55 -0400

    uncomment application.rb require 'sprockets/railtie'

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 require 'pagy/extras/array'
 require 'open-uri'
