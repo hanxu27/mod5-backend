127f605bde9c37d46f7de696d5301e03b149aa51
Han Xu <hanxu27@gmail.com>
2019-08-01 21:58:50 -0400

    commit out ENV in application.rb

--- config/application.rb
+++ config/application.rb
@@ -36,13 +36,13 @@ module Backend
     config.api_only = true
   end
 
-  config_files = ['secrets.yml']
+  # config_files = ['secrets.yml']
 
-  config_files.each do |file_name|
-    file_path = File.join(Rails.root, 'config', file_name)
-    config_keys = HashWithIndifferentAccess.new(YAML.safe_load(IO.read(file_path)))[Rails.env]
-    config_keys.each do |k, v|
-      ENV[k.upcase] ||= v
-    end
-  end
+  # config_files.each do |file_name|
+  #   file_path = File.join(Rails.root, 'config', file_name)
+  #   config_keys = HashWithIndifferentAccess.new(YAML.safe_load(IO.read(file_path)))[Rails.env]
+  #   config_keys.each do |k, v|
+  #     ENV[k.upcase] ||= v
+  #   end
+  # end
 end
